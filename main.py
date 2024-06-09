cc0cfabfd1fe164f3e8b825fc5c7e767e0d82365
mfurqanjaved <mfurqanjaved@gmail.com>
2024-06-09 10:30:55 +0500

    data validation added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from text_summariser.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from text_summariser.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from text_summariser.logging import logger
 
 
@@ -11,4 +12,15 @@ try:
 
 except Exception as e:
     logger.error(f"{STAGE_NAME} failed with error: {str(e)}")
+    raise e
+
+
+
+STAGE_NAME = "STAGE 02: DATA VALIDATION"
+try:
+    logger.info(f"{STAGE_NAME} started")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
     raise e
